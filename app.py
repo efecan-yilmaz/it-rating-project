f8307b7a447bad45cfa8644073ac088a83664bfa
Efecan YILMAZ <efecanyilmaz@outlook.com>
2025-07-03 16:31:51 +0200

    change default sidebar open behavior

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+st.set_page_config(layout="wide")
 
 pages = {
     "Step 1:": [st.Page("pages/tools.py", title="IT Tool Data Collection")],
