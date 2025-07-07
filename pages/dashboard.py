82e661c98129cf7631f77e026bff526de7f769bc
Efecan YILMAZ <efecanyilmaz@outlook.com>
2025-07-07 23:06:20 +0200

    image

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,4 +1,3 @@
 import streamlit as st
 
 st.image("assets/dashboard1.jpeg", use_container_width=True)
-st.image("assets/dashboard2.jpeg", use_container_width=True)
--- pages/requirement.py
+++ pages/requirement.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Requirement")
+st.title("Desigin Recommendation")
 
 st.write("This page is under construction. Please come back later for updates.")
 st.write("PLACEHOLDER FOR REQUIREMENT PAGE")
