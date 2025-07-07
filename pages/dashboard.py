1c61e5bbca585348af99965c07f70109cec992b7
Efecan YILMAZ <efecanyilmaz@outlook.com>
2025-07-07 23:21:00 +0200

    image

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.image("assets/dashboard1.jpeg", use_container_width=True)
+col1, col2 = st.columns([1, 1], gap="small", vertical_alignment="bottom")
+col1.image("assets/dashboard1.jpeg", use_container_width=True)
+col2.image("assets/dashboard2.jpeg", use_container_width=True)
+col1.image("assets/dashboard3.jpeg", use_container_width=True)
+col2.image("assets/dashboard4.jpeg", use_container_width=True)
